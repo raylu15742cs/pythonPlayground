cb33655623b7e850046d52aa8e1935a1e1b3a6ba
raylu15742cs <raylucs15742@gmail.com>
2022-12-12 17:27:31 -0800

    add insert at end

--- linkedlist.py
+++ linkedlist.py
@@ -30,6 +30,24 @@ class LinkedList:
         node = Node(data, self.head)
         self.head = node
     
+    def insert_at_end(self,data):
+        if self.head is None:
+            self.head = Node(data,None)
+            return
+        itr = self.head
+
+        while itr.next:
+            itr = itr.next
+        
+        itr.next = Node(data, None)
+    
+    def insert_at(self, index, data):
+        if index<0 or index > self.get_length():
+            raise Exception("Invalid Index")
+        if index == 0:
+            self.insert_at_begining(data)
+            return
+        
 
     
 
@@ -37,5 +55,5 @@ class LinkedList:
 if __name__ == '__main__':
     ll = LinkedList()
     ll.insert_at_begining(2)
-    ll.insert_at_begining(3)
+    ll.insert_at_end(3)
     ll.print()
